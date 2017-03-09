f74062958267de8c0cfc7d163eb66b4d3c3def4f
Eskender Memetov <skorpi76@gmail.com>
2017-03-09 15:36:44 -0400

    First commit
    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    //Public fields 
+    public float speed;
+
+    // Privat fields
+    private Rigidbody2D rb;
+
+    //Initialize our vars
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // when using physics use FixedUpdate, velocity = speed + direction    || Vecto2 = (x,y) 
+    void FixedUpdate () {
+      float moveHorizontal = Input.GetAxis("Horizontal");
+      float moveVertical = Input.GetAxis("Vertical");
+
+
+      Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+      rb.velocity = movement * speed;
+      rb.position = new Vector2(Mathf.Clamp(rb.position.x, -8f, 4f), Mathf.Clamp(rb.position.y, -4f,4f));
+    }
+}
